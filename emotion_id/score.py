cb07f73a45f5051478f4b14ecfe9cf7bc2cf660f
John Hughes <jpl.hughes@btinternet.com>
2020-06-07 17:17:27 +0100

    update scoring to get f1 scores and confusion matrices

--- emotion_id/score.py
+++ emotion_id/score.py
@@ -1,45 +1,69 @@
 import json
+import os
+import numpy as np
 from absl import flags, app
-
-from pyannote.core import Annotation, Segment
-from pyannote.metrics.identification import IdentificationErrorRate
+from sklearn.metrics import (
+    confusion_matrix,
+    f1_score,
+    precision_recall_fscore_support,
+    accuracy_score,
+)
+import seaborn as sns
+import matplotlib.pyplot as plt
 from emotion_id.dataset import get_emotion_to_id_mapping
 
 # This scoring assumes only one emotion per file in the reference
 flags.DEFINE_string("ref", None, "Path to reference emotions")
 flags.DEFINE_string("pred", None, "Path to predicted emotions")
+flags.DEFINE_string("output", None, "Path to predicted emotions")
 flags.DEFINE_string("emotion_set_path", None, "path to emotion set")
 flags.DEFINE_boolean("single", False, "Score only a single file")
+flags.DEFINE_list("actor_ignore", [], "actors to ignore in scoring")
 FLAGS = flags.FLAGS
 flags.mark_flag_as_required("ref")
 flags.mark_flag_as_required("pred")
+flags.mark_flag_as_required("output")
 flags.mark_flag_as_required("emotion_set_path")
 
 
-def emotion_timings_to_annotation(input_emotions):
+def get_stats(refs, preds, emotion2id, acc_type):
     """
-    Converts list of emotions and timings into a pyannote segmentation
+    :param refs: List of reference emotion classes
+    :param preds: List of predicted emotion classes
+    :param acc_type: String used in confusion matrix png name
     """
-    result = Annotation()
-    for event in input_emotions:
-        start, end, emotion = event
-        result[Segment(start, end)] = emotion
-    return result
-
-
-def single_file_stats(ref_annotation, pred_annotation):
-    """
-    :param ref_annotation: reference annotation in pyannote format
-    :param pred_annotation: prediction annotation in pyannote format
-    :return: a dict containing results
-    """
-    IER = IdentificationErrorRate()
-    components = IER.compute_components(
-        ref_annotation, pred_annotation, uem=ref_annotation.get_timeline()
+    cm = confusion_matrix(refs, preds)
+    f1_scores = f1_score(refs, preds, average=None)
+    precisions, recalls, _, _ = precision_recall_fscore_support(
+        refs, preds, average=None
     )
-    hits = components["correct"]
-    misses = components["total"] - hits
-    results = {"overall": {"hits": hits, "misses": misses}}
+    acc = accuracy_score(refs, preds)
+    print(f"Frame-wise accuracy: {acc:.4f}")
+    results = {
+        "accuracy": acc,
+    }
+
+    for f1, precision, recall, emotion in zip(
+        f1_scores, precisions, recalls, emotion2id.keys()
+    ):
+        print(f"{emotion}, precision={precision:.3f}, recall={recall:.3f}, f1={f1:.3f}")
+        results[emotion] = {
+            "precision": precision,
+            "recall": recall,
+            "f1": f1,
+        }
+    print(cm)
+    np.savetxt(f"{FLAGS.output}/confusion_matrix_{acc_type}.txt", cm)
+    np.save(f"{FLAGS.output}/confusion_matrix_{acc_type}.npy", cm)
+
+    ax = plt.subplot()
+    sns.heatmap(cm, annot=False, ax=ax)
+    ax.set_xlabel("Predicted labels")
+    ax.set_ylabel("True labels")
+    ax.set_title("Confusion Matrix")
+    plt.savefig(f"{FLAGS.output}/confusion_matrix_{acc_type}.png")
+    plt.clf()
+
     return results
 
 
@@ -65,35 +89,39 @@ def overall_stats(ref, pred, emotion_set_path, single=False):
 
     # loop over files, gathering results
     results = {}
+
+    all_preds = []
+    all_refs = []
+    file_preds = []
+    file_refs = []
     for ref_emotion, pred_file in zip(ref_emotions, pred_files):
+        actor = pred_file.split("-")[-1].split(".")[0]
+        if actor in FLAGS.actor_ignore:
+            continue
         emotion_id = emotion2id[ref_emotion]
-        pred_emotions = []
+        frame_preds = []
         with open(pred_file) as in_f:
             for line in in_f:
-                items = line.strip().split()
-                pred_emotions.append((float(items[0]), float(items[1]), int(items[2])))
-        ref_emotions = [(0.0, pred_emotions[-1][1], emotion_id)]
+                frame_pred = int(line.strip().split()[2])
+                frame_preds.append(frame_pred)
+                all_refs.append(emotion_id)
+        all_preds.extend(frame_preds)
+        counts = np.bincount(np.array(frame_preds))
+        file_preds.append(np.argmax(counts))
+        file_refs.append(emotion_id)
 
-        ref_annotation = emotion_timings_to_annotation(ref_emotions)
-        pred_annotation = emotion_timings_to_annotation(pred_emotions)
+    results["frame_wise"] = get_stats(all_refs, all_preds, emotion2id, "frame_wise")
+    results["file_wise"] = get_stats(file_refs, file_preds, emotion2id, "file_wise")
 
-        nu_results = single_file_stats(ref_annotation, pred_annotation)
-        if len(results) == 0:
-            results = nu_results
-        else:
-            for sublevel in nu_results:
-                for item in results[sublevel]:
-                    results[sublevel][item] += nu_results[sublevel][item]
-    for sublevel in results:
-        results[sublevel]["accuracy"] = float(results[sublevel]["hits"]) / (
-            results[sublevel]["hits"] + results[sublevel]["misses"]
-        )
     return results
 
 
 def score(unused_argv):
+    os.makedirs(FLAGS.output, exist_ok=True)
     results = overall_stats(FLAGS.ref, FLAGS.pred, FLAGS.emotion_set_path, FLAGS.single)
-    print(json.dumps(results))
+    print(json.dumps(results, indent=4))
+    with open(f"{FLAGS.output}/score_results.json", "w") as outfile:
+        json.dump(results, outfile)
 
 
 if __name__ == "__main__":
